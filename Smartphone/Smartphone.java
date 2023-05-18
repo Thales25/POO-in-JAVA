0b070cf5cc3d3ea9a3570153ba446ad5d3b74f58
Thales25 <thalessousa8579@gmail.com>
2023-05-18 11:15:29 -0300

    adicionando o metodo printstatus

--- Smartphone/Smartphone.java
+++ Smartphone/Smartphone.java
@@ -7,26 +7,29 @@ public class Smartphone {
     String processador;
     String sistemaOperacional;
 
+    public void printStatus() {
+        System.out.println("*******************************");
+        System.out.println("* Modelo: " + modelo);
+        System.out.println("* Cor: " + cor);
+        System.out.println("* Memória de Armazenamento: " + memoriaArmazenamento);
+        System.out.println("* Memória RAM: " + memoriaRam);
+        System.out.println("* Processador: " + processador);
+        System.out.println("* Câmera: " + camera);
+        System.out.println("* Sistema Operacional: " + sistemaOperacional);
+        System.out.println("*******************************");
+    }
 
-public static void main (String [] args){
+    public static void main(String[] args) {
+        Smartphone smart1 = new Smartphone();
 
-    Smartphone smart1= new Smartphone();
+        smart1.modelo = "Motorola Edge";
+        smart1.cor = "Vermelho";
+        smart1.memoriaArmazenamento = "64 Gb";
+        smart1.memoriaRam = "4 Gb";
+        smart1.processador = "Octa Core";
+        smart1.camera = "64 mpx";
+        smart1.sistemaOperacional = "Android";
 
-    smart1.modelo = "Motorola Edge"; 
-    smart1.cor = "Vermelho";
-    smart1.memoriaArmazenamento = "64 Gb";
-    smart1.memoriaRam = "4 Gb";
-    smart1.processador= "Octa Core";
-    smart1.camera= "64 mpx";
-    smart1.sistemaOperacional = "Android";
-
-System.out.println("modelo: "+smart1.modelo);
-System.out.println("cor: "+smart1.cor);
-System.out.println("Armazenamento: "+smart1.memoriaArmazenamento);
-System.out.println("RAM: "+smart1.memoriaRam);
-System.out.println("Processador: "+smart1.processador);
-System.out.println("Mpx da Camera: "+smart1.camera);
-System.out.println("Sistema Operacional: "+smart1.sistemaOperacional);
+        smart1.printStatus();
     }
 }
-
